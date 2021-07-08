47a14811f89d6f4e78dd2ed45f24045b50af2269
pampanaharish <harishasry@gmail.com>
2021-07-08 15:44:33 +0300

    First commit

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import products from "../products";
+
+export default function Homescreen() {
+  return (
+    <div>
+      <h1>Home screen page</h1>
+      <h1>'{typeof products.lenght}'</h1>
+    </div>
+  );
+}
